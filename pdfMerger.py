b7e1dca66ce286deafb014da4c1877b167138bb7
Jimmy Phan <jimmyphan1@hotmail.com>
2023-02-03 18:34:15 -0500

    Updated Files

--- pdfMerger.py
+++ pdfMerger.py
@@ -6,10 +6,10 @@ import os
 merger = PyPDF2.PdfMerger()
 
 # This program will iterate through the current directory in a for loop
-# And combine the documents in order of 
+# And combine the documents in order from top to bottom
 
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         merger.append(file)
-    # The name below will be the new name
+    # The name below will be the new name, change if needed
     merger.write("CombinedPDF.pdf")
